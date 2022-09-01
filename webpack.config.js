b21c0d4718d493c572eacfff53bf3e08e81ab95a
eas <eas@mailbox.org>
2022-09-01 05:14:05 +0200

    Update webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   module: {
     rules: [
       {
